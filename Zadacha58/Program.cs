f832d6e0f3ffc7c5dc38ebbfa7e9bf9e81333a56
maxbondarenko <bondarenkomm@bk.ru>
2022-12-14 19:15:00 +0500

    Zadacha58

---
+++ Zadacha58/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте две квадратные матрицы одного размера. 
+// Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int[,] firstMartrix = {{2,4},{3,2}};
+int[,] secondMartrix = {{3,4},{3,3}};
+int[,] resultMatrix = new int[2,2];
+
+WriteArray(firstMartrix);
+WriteArray(secondMartrix);
+MultiplyMatrix(firstMartrix, secondMartrix, resultMatrix);
+WriteArray(resultMatrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
+void MultiplyMatrix(int[,] firstMartrix, int[,] secondMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secondMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
